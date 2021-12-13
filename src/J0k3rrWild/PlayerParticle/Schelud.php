fa5215d862b28814ff0d0ffa8ee387d3d51af952
J0k3rrWild <alanpol857@gmail.com>
2021-12-13 22:23:06 +0000

    Removed help comments for pmmp tasks

--- src/J0k3rrWild/PlayerParticle/Schelud.php
+++ src/J0k3rrWild/PlayerParticle/Schelud.php
@@ -1,21 +1,20 @@
-<?php // As always when you start a PHP file
+<?php 
 
-namespace J0k3rrWild\PlayerParticle; // Use the same namespace as in your first file but add a \tasks who symbolise that this file is in the subfolder "tasks"
+namespace J0k3rrWild\PlayerParticle; 
 
-use pocketmine\scheduler\Task; // This is the class that your task will extends to be a plugin task
-use J0k3rrWild\PlayerParticle\Main; // This will allow us to use our main class. It is a required argument for a plugin task.
+use pocketmine\scheduler\Task;
 use pocketmine\level\particle\FloatingTextParticle; 
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
 
 
 
-class Schelud extends Task{ // Remember that your task must have the same name as your file !
+class Schelud extends Task{
 
-// First we need a __construct function which is used when you create a class to set default variables, ect...
-    public function __construct(Main $plugin, $player){ // The arguments you define here depends on what do you want to do exept for your base.
-       $this->plugin = $plugin; //You can retrieve your main class at anytime and use it's methods on your class by using $this->getOwner()
-       $this->player = $player; // So we can retreive the player for later.
+
+    public function __construct(Main $plugin, $player){ 
+       $this->plugin = $plugin; 
+       $this->player = $player; 
        
     } 
 
